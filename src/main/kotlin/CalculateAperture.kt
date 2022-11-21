2d35c1446f85815b4ea023a38885e7dfb5cdeeb1
guilhermealbm <gui.miranda.13@gmail.com>
2022-11-21 18:44:24 -0300

    update CalculateAperture.kt

--- src/main/kotlin/CalculateAperture.kt
+++ src/main/kotlin/CalculateAperture.kt
@@ -1,11 +1,25 @@
+import kotlin.math.abs
+
+val distances = listOf(1.2, 1.8, 2.5, 3.5, 5.0, 7.0)
+
 fun calculateAperture(iso_: String?, distance_: String?) : String {
     val iso = convertIso(iso_)
-    distance_?.toDouble()?.let {
+    val distance = convertDistance(distance_)
+    distance?.let {
         return ""
     } ?: run {
         return "Unable to convert distance."
     }
 }
+
+fun convertDistance(distance_: String?) : Double? =
+    distance_?.toDouble()?.let {
+        distances.closestValue(it)
+    } ?: run {
+        null
+    }
+
+private fun List<Double>.closestValue(value: Double) = minByOrNull { abs(value - it) }
 
 fun convertIso(iso: String?) : ISO =
     when(iso?.trim()) {
